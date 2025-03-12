9e063b8ad4ac6b29a7d2ccf6cf4a51e84a4798b0
Viktor web2025-3 <victorshegelsky@gmail.com>
2025-03-13 00:59:20 +0200

    finished part 1

---
+++ main.js
@@ -0,0 +1,35 @@
+const { program } = require('commander');
+const fs = require('fs');
+
+program
+  .option('-i, --input <type>', 'path to input file (required)', String)
+  .option('-o, --output <type>', 'path to output file (optional)', String)
+  .option('-d, --display', 'display result in console (optional)')
+  .parse(process.argv);
+
+const options = program.opts();
+
+if (!options.input) {
+  console.error("Please, specify input file");
+  process.exit(1);
+}
+
+if (!fs.existsSync(options.input)) {
+  console.error("Cannot find input file");
+  process.exit(1);
+}
+
+const inputData = JSON.parse(fs.readFileSync(options.input, 'utf8'));
+
+if (options.output && options.display) {
+  console.log(inputData);
+  fs.writeFileSync(options.output, JSON.stringify(inputData, null, 2), 'utf8');
+}
+
+if (options.display) {
+  console.log(inputData);
+}
+
+if (options.output) {
+  fs.writeFileSync(options.output, JSON.stringify(inputData, null, 2), 'utf8');
+}
